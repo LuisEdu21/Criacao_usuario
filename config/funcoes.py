66cd15b5d597abbb0cf7ee848d0d9b5282c096b7
LuisEdu21 <luiseduardotgs2001@hotmail.com>
2024-03-30 23:26:21 -0300

    Criacao da tabela e verificar se ela existe

--- config/funcoes.py
+++ config/funcoes.py
@@ -18,7 +18,7 @@ def Fecha_conexao(conn_postgresql, cursor_postgresql):
 
     return
 
-def usuario():
+def usuarios():
     url = "https://randomuser.me/api/"
 
     payload = {}
@@ -30,21 +30,48 @@ def usuario():
 
     return
 
+def tabela_existe(cursor_postgresql):
+    
+    sql = """
+    SELECT EXISTS (
+        SELECT 1
+        FROM information_schema.tables 
+        WHERE table_name = 'cliente_teste'
+    );
+    """
+    cursor_postgresql.execute(sql)
+    return cursor_postgresql.fetchone()[0]
+
+def criar_tabela(conn_postgresql, cursor_postgresql):
+
+    tabela = """
+    CREATE TABLE teste.cliente_teste (
+        id serial4 NOT NULL,
+        name_first varchar NULL,
+        name_last varchar NULL,
+        city varchar NULL,
+        state varchar NULL,
+        country varchar NULL,
+        email varchar NULL,
+        data_nascimento timestamp NULL,
+        age int NULL
+    );
+    """
+
+    if tabela_existe(cursor_postgresql):
+        return 'A tabela já existe'
+    else:
+        cursor_postgresql.execute(tabela)
+        conn_postgresql.commit()
+        return 'Tabela criada'
+
 def run():
 
     conn_postgresql = Abrir_conexao()
     cursor_postgresql = conn_postgresql.cursor()
 
-    # Execute uma consulta SQL para obter a versão do banco de dados
-    cursor_postgresql.execute('SELECT version()')
+    criar_tabela(conn_postgresql=conn_postgresql,cursor_postgresql=cursor_postgresql)
 
-    # Recupere o resultado da consulta
-    versao = cursor_postgresql.fetchone()
-
-    # Imprima a versão do banco de dados
-    print('Versão do banco de dados:', versao[0])
-
+    usuario = usuarios()
 
     return
-
-run()
